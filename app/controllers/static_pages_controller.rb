3f4ad76cbdc6fa0e0f7bde452a4c1c2227b047b8
Blitzkriegtech <aljunrendon@yahoo.com>
2025-09-22 01:03:57 -0400

    Add a heroo template in home view.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
   def index
   end
+
+  def home
+  end
+
+  def help
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "/home", to: "static_pages#home"
+  get "/help", to: "static_pages#help"
   root "static_pages#index"
 end
